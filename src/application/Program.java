d4a40435a3c31d595381b442f342e7b31e178216
Lavinya Correia <lavinya.correia01@gmail.com>
2020-04-14 08:53:41 -0300

    Coloring possible moves

--- src/application/Program.java
+++ src/application/Program.java
@@ -23,6 +23,10 @@ public class Program {
 			System.out.print("Source: ");
 			ChessPosition source = UI.readChessPosition(sc);
 			
+			boolean[][] possibleMoves = chessmatch.possibleMoves(source);
+			UI.clearScreen();
+			UI.printBoard(chessmatch.getPieces(), possibleMoves);		
+			
 			System.out.println();
 			System.out.print("Target: ");
 			ChessPosition target = UI.readChessPosition(sc);
